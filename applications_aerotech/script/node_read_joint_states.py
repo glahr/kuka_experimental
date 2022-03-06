e0261767b775c32e344a36aec752e64af7a7d0bb
Leonardo Simião <leonardosimiaol@gmail.com>
2022-03-05 22:09:55 -0300

    feat: publish force and torque sensor data to mqtt on topic ft_sensor

--- applications_aerotech/script/node_read_joint_states.py
+++ applications_aerotech/script/node_read_joint_states.py
@@ -7,13 +7,14 @@ import numpy as np
 import sensor_msgs.msg
 import control_msgs.msg
 import trajectory_msgs.msg
+import geometry_msgs.msg
 from std_msgs.msg import String, Float64, Float64MultiArray
 
 log_joints_desired = []
 log_joints_actual = []
 log_joints_error = []
 log_timestamp = []
-log_xml = []
+log_ft = []
 
 
 def callback(joint_states):
@@ -36,16 +37,23 @@ def callback(joint_states):
     mqtt_publisher(str(joint_states.status.goal_id.stamp), String, 'timestamp')
 
 
+def ft_callback(ft_sensor_data):
+    log_ft.append([ft_sensor_data.wrench.force.x, ft_sensor_data.wrench.force.y, ft_sensor_data.wrench.force.z,
+                      ft_sensor_data.wrench.torque.x, ft_sensor_data.wrench.torque.y, ft_sensor_data.wrench.torque.z])
+    mqtt_publisher(ft_sensor_data, geometry_msgs.msg.WrenchStamped, 'ft_sensor')
+
+
 def xml_writer(data):
     # rospy.loginfo("I heard %s", data)
     log_xml.append([data])
+
 
 def listener():
 
     rospy.init_node('listener', anonymous=True)
 
     # rospy.Subscriber("/joint_states", sensor_msgs.msg.JointState, callback)
-    rospy.Subscriber("/rsi_xml_doc", String, xml_writer)
+    rospy.Subscriber("/ft_sensor_data", geometry_msgs.msg.WrenchStamped, ft_callback)
     rospy.Subscriber("/position_trajectory_controller/follow_joint_trajectory/feedback", control_msgs.msg.FollowJointTrajectoryActionFeedback, callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
@@ -58,12 +66,6 @@ def mqtt_publisher(message, message_type, topic):
         message = Float64MultiArray()
         message.data = [float(j) for j in message_content]
 
-    if message_type == control_msgs.msg.FollowJointTrajectoryFeedback:
-        message_content = message
-        message = control_msgs.msg.FollowJointTrajectoryFeedback()
-        message.desired.positions = [float(j) for j in message_content.desired.positions]
-        message.actual.positions = [float(j) for j in message_content.actual.positions]
-        message.error.positions = [float(j) for j in message_content.error.positions]
     pub = rospy.Publisher(topic, message_type, queue_size=10)
     pub.publish(message)
 
@@ -93,15 +95,15 @@ if __name__ == '__main__':
         for item in log_timestamp:
             string_write = str(item) + '\n'
             ft.write(str(string_write))
-    with open('rsi_xml_doc.txt', 'w') as fx:
-        # fd.write('a1, a2, a3, a4, a5, a6\n')
-        for item in log_xml:
-            string_write = str(item) + '\n'
-            fx.write(str(string_write))
+    with open('ft_sensor.txt', 'w') as fft:
+        fe.write('Fx, Fy, Fz, Mx, My, Mz\n')
+        for item in log_ft:
+            string_write = str(item[0]) + ',' + str(item[1]) + ',' + str(item[2]) + ',' + str(item[3]) + ',' + str(item[4]) + ',' + str(item[5]) + '\n'
+            fft.write(str(string_write))
 
     fd.close()
     fe.close()
     fa.close()
     ft.close()
-    fx.close()
+    fft.close()
     print("SALVO")
